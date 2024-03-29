9b1181102dfa30f59f38700b0a84ec84dc970c11
JBallesteros <jay.d.awsome@gmail.com>
2024-03-28 21:11:51 -1000

    contact forms finished in 36 minutes

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -13,50 +13,50 @@ const NavBar = () => {
   }), []);
 
   return (
-    <Navbar bg="dark" expand="lg">
-      <Container>
-        <Navbar.Brand as={NavLink} to="/">
-          <h2>digits</h2>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto justify-content-start">
-            {currentUser ? ([
-              <Nav.Link id="add-stuff-nav" as={NavLink} to="/add" key="add">Add Stuff</Nav.Link>,
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/list" key="list">List Contacts</Nav.Link>,
-            ]) : ''}
-            {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-              <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin" key="admin">Admin</Nav.Link>
-            ) : ''}
-          </Nav>
-          <Nav className="justify-content-end">
-            {currentUser === '' ? (
-              <NavDropdown id="login-dropdown" title="Login">
-                <NavDropdown.Item id="login-dropdown-sign-in" as={NavLink} to="/signin">
-                  <PersonFill />
-                  Sign
-                  in
-                </NavDropdown.Item>
-                <NavDropdown.Item id="login-dropdown-sign-up" as={NavLink} to="/signup">
-                  <PersonPlusFill />
-                  Sign
-                  up
-                </NavDropdown.Item>
-              </NavDropdown>
-            ) : (
-              <NavDropdown id="navbar-current-user" title={currentUser}>
-                <NavDropdown.Item id="navbar-sign-out" as={NavLink} to="/signout">
-                  <BoxArrowRight />
-                  {' '}
-                  Sign
-                  out
-                </NavDropdown.Item>
-              </NavDropdown>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar bg="dark" expand="lg">
+        <Container>
+          <Navbar.Brand as={NavLink} to="/">
+            <h2>digits</h2>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto justify-content-start">
+              {currentUser ? ([
+                <Nav.Link id="add-stuff-nav" as={NavLink} to="/add" key="add">Add Contact</Nav.Link>,
+                <Nav.Link id="list-stuff-nav" as={NavLink} to="/list" key="list">List Contacts</Nav.Link>,
+              ]) : ''}
+              {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+                  <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin" key="admin">Admin</Nav.Link>
+              ) : ''}
+            </Nav>
+            <Nav className="justify-content-end">
+              {currentUser === '' ? (
+                  <NavDropdown id="login-dropdown" title="Login">
+                    <NavDropdown.Item id="login-dropdown-sign-in" as={NavLink} to="/signin">
+                      <PersonFill />
+                      Sign
+                      in
+                    </NavDropdown.Item>
+                    <NavDropdown.Item id="login-dropdown-sign-up" as={NavLink} to="/signup">
+                      <PersonPlusFill />
+                      Sign
+                      up
+                    </NavDropdown.Item>
+                  </NavDropdown>
+              ) : (
+                  <NavDropdown id="navbar-current-user" title={currentUser}>
+                    <NavDropdown.Item id="navbar-sign-out" as={NavLink} to="/signout">
+                      <BoxArrowRight />
+                      {' '}
+                      Sign
+                      out
+                    </NavDropdown.Item>
+                  </NavDropdown>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
   );
 };
 
